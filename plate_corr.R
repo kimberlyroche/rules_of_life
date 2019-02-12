fbeee778545d7f918eaceb8947bdf15a7dd59da0
Kimberly Roche <ker48@c1-01-4.genome.duke.edu>
2019-02-12 13:29:33 -0500

    Added script to crudely test correlation of features

--- plate_corr.R
+++ plate_corr.R
@@ -2,17 +2,51 @@
 #filtered <- filter_data(sample_threshold=0.9)
 #md <- read_metadata(filtered)
 
-limit <- 5000
+limit <- 1000
 sample_idx <- sample(nsamples(filtered))[1:limit]
 
-y <- as.factor(md$plate)[sample_idx]
-y2 <- md$plate[sample_idx]
+plate <- md$plate[sample_idx]
 
-#x <- unlist(lapply(md$collection_date, function(x) as.integer(format(as.Date(x), "%Y%m%d"))))[sample_idx]
-x <- as.factor(md$sname)[sample_idx]
+covar <- c("season", "date", "grp", "sname")
 
-png("test.png")
-plot(x, y, xlab="date", ylab="plate")
-dev.off()
+for(covariate in covar) {
+  if(covariate == "date") {
+    cov_data <- unlist(lapply(md$collection_date, function(x) as.integer(format(as.Date(x), "%Y%m%d"))))[sample_idx]
+  } else if(covariate == "season") {
+    cov_data <- md$season[sample_idx]
+  } else if(covariate == "grp") {
+    cov_data <- md$grp[sample_idx]
+  } else if(covariate == "sname") {
+    cov_data <- md$sname[sample_idx]
+  }
 
-#cat("Spearman cor:",cor(x, y2, method="spearman"),"\n")
+  data <- data.frame(plate=scale(plate), covariate=cov_data)
+
+  # fit linear model; ask about percent variance explained
+  if(covariate == "date") {
+    res <- lm(plate~cov_data, data)
+  } else {
+    res <- lm(plate~factor(cov_data), data)
+  }
+  cat("Variance explained by",covariate,":",(1-var(res$residuals)),"\n")
+  #cat("Check with model:",summary(res)$r.squared,"\n")
+  #print(res)
+
+  if(covariate == "date") {
+    p <- data %>% ggplot(aes(x=plate, y=covariate)) +
+      geom_point(size=1) +
+      theme_minimal() +
+      xlab("plate") +
+      ylab(covariate)
+    ggsave(paste("testcov_",covariate,".png",sep=""), plot=p, scale=1.5, width=3, height=3, units="in")
+  }
+}
+
+# how well does individual predict date anyway?
+date <- unlist(lapply(md$collection_date, function(x) as.integer(format(as.Date(x), "%Y%m%d"))))[sample_idx]
+date <- scale(date)
+indiv <- md$sname[sample_idx]
+data <- data.frame(x=indiv, y=date)
+res <- lm(y~factor(x), data)
+cat("Variance in collection_date explained by sname :",(1-var(res$residuals)),"\n")
+#cat("Check with model:",summary(res)$r.squared,"\n")
