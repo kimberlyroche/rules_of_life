bea9e4fd52c4f4d3cfdb60523359dec9a7ee393d
Kim Roche <kimberly.e.roche@gmail.com>
2019-06-19 13:22:30 -0400

    Update to basset workup for baboon data

--- basset_walkthru.r
+++ basset_walkthru.r
@@ -2,7 +2,7 @@ library(stray)
 library(phyloseq)
 library(dplyr)
 library(driver)
-library(ggplot2)
+library(tidyverse)
 
 # X is Q x N as in other kernels
 # bandwidth: rho as chosen gives antiphase observations a correlation of ~0.1
@@ -12,106 +12,6 @@ PER <- function(X, sigma=1, rho=1, period = 24, jitter = 1e-10){
   return(G)
 }
 
-# returns fit (*LR)
-fit_to_reference <- function(Gamma){
-  data(mallard) # loads "ps" phyloseq obj
-  
-  # massage data
-  ps <- prune_samples(sample_data(ps)$Vessel==1, ps) # vessel 1
-  ps <- prune_samples((sample_data(ps)$time > "2015-11-20 15:00:00 UTC") &
-                        (sample_data(ps)$time < "2015-11-25 16:00:00 UTC"), ps) # hourly samples only
-  o <- order(sample_data(ps)$time) # get the order of samples
-  otu_table(ps) <- otu_table(ps)[o,] # extract counts in this order (samples are rows!)
-  sample_data(ps) <- sample_data(ps)[o,] # extract sample data in this order (samples are rows!)
-  Y <- t(as(otu_table(ps), "matrix")) # extract counts; D x N
-  rownames(Y) <- taxa_names(ps) # format: seq_*
-
-  D <- ntaxa(ps)
-  N <- nrow(sample_data(ps))
-  
-  upsilon <- D-1+3
-  Xi <- matrix(.4, D-1, D-1)
-  diag(Xi) <- 1
-  Theta <- function(X) matrix(0, D-1, ncol(X))
-  
-  X <- as.numeric(sample_data(ps)$time) # hour identifier
-  X <- t((X-min(X)) / 3600) # readable hours 0..115
-  
-  fit <- stray::basset(Y, X, upsilon, Theta, Gamma, Xi)
-  fit.clr <- to_clr(fit)
-
-  family_names <- as(tax_table(ps)[,"Family"], "vector")
-
-  return(list(Y=Y, X=X, fit=fit.clr, labels=as(tax_table(ps)[,"Family"], "vector")))
-}
-
-fit_to_baboon <- function(Gamma, sname, start_date_str, end_date_str) {
-  source("include.R")
-  glom_data <- load_glommed_data(level="family", replicates=TRUE)
-  filtered <- filter_data(glom_data, count_threshold=10, sample_threshold=0.66, verbose=TRUE)
-  # family-agglomerated has replicates; remove these
-  non_reps <- prune_samples(sample_data(filtered)$sample_status==0, filtered)
-  cat("Filtered down to",ntaxa(non_reps),"taxa\n")
-  
-  pruned <- prune_samples(sample_data(non_reps)$sname==sname, non_reps)
-  pruned <- prune_samples((sample_data(pruned)$collection_date > start_date_str) &
-                        (sample_data(pruned)$collection_date < end_date_str), pruned)
-  cat("Using",phyloseq::nsamples(pruned),"samples from",sname,"\n")
-  tax <- tax_table(pruned)
-  o <- order(sample_data(pruned)$collection_date)
-  otu_table(pruned) <- otu_table(pruned)[o,]
-  sample_data(pruned) <- sample_data(pruned)[o,]
-  Y <- t(as(otu_table(pruned), "matrix")) # dimensions D x N
-  readable_rownames <- c()
-  base_names_used <- list()
-  for(t in taxa_names(pruned)) {
-    # we need these identifiers to be unique, so if family is unknown (frequently)
-    # backtrack to find some usable identifier
-    tax_lvl <- 5
-    tax_label <- as(tax[t,], "vector")[tax_lvl]
-    # if name is already taken, just append a counter to it
-    if(tax_label %in% names(base_names_used)) {
-      base_names_used[[tax_label]] = base_names_used[[tax_label]] + 1
-      tax_label <- paste0(tax_label, " (", base_names_used[[tax_label]], ")")
-    } else {
-      # otherwise if it's NA, move up phylogenetic tree until we hit something concrete
-      while(is.na(tax_label)) {
-        tax_lvl <- tax_lvl - 1
-        tax_label <- as(tax[t,], "vector")[tax_lvl]
-      }
-      if(tax_lvl == 0 && is.na(tax_label)) {
-        tax_label <- "unknown"
-      }
-      if(tax_label %in% names(base_names_used)) {
-        base_names_used[[tax_label]] = base_names_used[[tax_label]] + 1
-        tax_label <- paste0(tax_label, " (", base_names_used[[tax_label]], ")")
-      } else {
-        base_names_used[[tax_label]] = 1
-      }
-    }
-    readable_rownames <- c(readable_rownames, tax_label)
-  }
-  rownames(Y) <- readable_rownames
-  
-  D <- ntaxa(pruned)
-  N <- nrow(sample_data(pruned))
-  
-  upsilon <- D-1+3
-  Xi <- matrix(.4, D-1, D-1)
-  diag(Xi) <- 1
-  Theta <- function(X) matrix(0, D-1, ncol(X))
-  
-  X_raw <- sample_data(pruned)$collection_date
-  # sorted, so min date is first
-  X <- sapply(X_raw, function(Z) { round(difftime(Z, X_raw[1], units="days")) } )
-  dim(X) <- c(1, length(X))
-  
-  fit <- stray::basset(Y, X, upsilon, Theta, Gamma, Xi)
-  #fit.clr <- to_clr(fit)
-  fit.alr <- to_alr(fit, D)
-  return(list(Y=Y, X=X, fit=fit.alr, labels=readable_rownames))
-}
-
 get_predictions <- function(X, fit){
   cat("Predicting from 1 to",max(X),"\n")
   X_predict <- t(1:(max(X))) # time point range, fill in any missing
@@ -119,53 +19,98 @@ get_predictions <- function(X, fit){
   return(list(X_predict=X_predict, Y_predict=predicted))
 }
 
-plot_fit <- function(Y, X, labels, Y_predict, X_predict, save_filename){
-  #Y_clr_tidy <- clr_array(Y+0.65, parts = 1) %>% 
-  #  gather_array(mean, coord, sample) %>% 
-  #  mutate(time = X[1,sample], 
-  #         coord = paste0("CLR(", labels[coord],")"))
-  Y_alr_tidy <- alr_array(Y+0.65, parts = 1) %>% 
-    gather_array(mean, coord, sample) %>% 
-    mutate(time = X[1,sample], 
-           coord = paste0("ALR_D(", labels[coord],")"))
+fit_to_baboon <- function(baboon, indiv_data, Gamma, date_lower_limit="2001-10-01", date_upper_limit="2002-11-30") {
+  Y_full <- indiv_data$ys
+  observations_full <- indiv_data$observation_vec
   
-  predicted_tidy <- gather_array(Y_predict, val, coord, sample, iter) %>% 
-    mutate(time = X_predict[1,sample]) %>% # just adds time point ID
-    filter(!is.na(val)) %>% # kill NAs
-    group_by(time, coord) %>% # there are D x N combos of time and coordinate (log ratio)
-    summarise_posterior(val, na.rm=TRUE) %>% # gets standard quantiles for each coord x timepoint combo
-    ungroup() %>% # just undoes this group x coord binding
-    mutate(coord = paste0("ALR_D(", labels[coord],")")) # replace coord with readable family name
+  if(!is.null(date_lower_limit) & !is.null(date_upper_limit)) {
+    # require both be present for now
+    min_idx <- min(which(names(observations_full) >= date_lower_limit))
+    max_idx <- max(which(names(observations_full) <= date_upper_limit))
+    
+    # subset dates
+    Y <- t(Y_full[min_idx:max_idx,])
+    colnames(Y) <- NULL; rownames(Y) <- NULL
+    # set first observation to t=1
+    observations <- matrix(observations_full[min_idx:max_idx], nrow=1) - observations_full[min_idx] + 1
+  } else {
+    Y <- t(Y_full)
+    colnames(Y) <- NULL
+    rownames(Y) <- NULL
+    observations <- matrix(observations_full, nrow=1)
+  }
+
+  D <- nrow(Y)
+  N <- ncol(Y)
   
-  p <- ggplot(predicted_tidy, aes(x = time, y=mean)) +
+  # ALR prior covariance
+  upsilon <- D-1+10 # lesser certainty
+  # supsilon <- D-1+20 # greater certainty; this should tighten the distribution around this mean
+  GG <- cbind(diag(D-1), -1) # log contrast for ALR with last taxon as reference;
+  # take diag as covariance over log abundances
+  Xi <- GG%*%(diag(D)*1)%*%t(GG)
+  # mean-center
+  Xi <- Xi*(upsilon-D-1)
+  
+  Theta <- function(X) matrix(1, D-1, ncol(X))
+
+  fit <- stray::basset(Y, observations, upsilon, Theta, Gamma, Xi)
+  #fit.clr <- to_clr(fit)
+  fit.alr <- to_alr(fit, D)
+  return(list(Y=Y, X=observations, fit=fit.alr))
+}
+
+plot_predictions <- function(fit_obj, predict_obj, LR_coord=1) {
+  Y.alr <- driver::alr(t(fit_obj$Y) + 0.5)
+  observations <- fit_obj$X
+  alr_tidy <- gather_array(Y.alr, "LR_value", "timepoint", "LR_coord")
+  
+  # replace timepoints with observation dates
+  map <- data.frame(timepoint=1:length(observations), observation=c(observations))
+  alr_tidy <- merge(alr_tidy, map, by="timepoint")
+  alr_tidy <- alr_tidy[,!(names(alr_tidy) %in% c("timepoint"))]
+
+  no_samples <- dim(predict_obj$Y_predict)[3]
+  posterior_samples <- gather_array(predict_obj$Y_predict[LR_coord,,], "LR_value", "observation", "sample_no")
+  # get quantiles
+  
+  post_quantiles <- posterior_samples %>%
+    group_by(observation) %>%
+    summarise(p2.5 = quantile(LR_value, prob=0.025),
+              p5 = quantile(LR_value, prob=0.05),
+              p10 = quantile(LR_value, prob=0.1),
+              p25 = quantile(LR_value, prob=0.25),
+              p50 = quantile(LR_value, prob=0.5),
+              mean = mean(LR_value),
+              p75 = quantile(LR_value, prob=0.75),
+              p90 = quantile(LR_value, prob=0.9),
+              p95 = quantile(LR_value, prob=0.95),
+              p97.5 = quantile(LR_value, prob=0.975)) %>%
+    ungroup()
+
+  p <- ggplot(post_quantiles, aes(x=observation, y=mean)) +
     geom_ribbon(aes(ymin=p2.5, ymax=p97.5), fill="darkgrey", alpha=0.5) +
     geom_ribbon(aes(ymin=p25, ymax=p75), fill="darkgrey", alpha=0.9) +
-    geom_line(color="blue") +
-    geom_point(data = Y_alr_tidy, alpha=0.5) +
-    facet_wrap(~coord, scales="free_y") +
-    theme_minimal() +
+    geom_line(color="blue") + 
+    geom_point(data=alr_tidy[alr_tidy$LR_coord==LR_coord,], aes(x=observation, y=LR_value), alpha=0.5) +
+    theme_minimal() + 
     theme(axis.title.y = element_blank(), 
           axis.title.x = element_blank(), 
           axis.text.x = element_text(angle=45))
-  ggsave(paste0("plots/",save_filename,".png",sep=""), plot=p, scale=1.5, width=8, height=5, units="in")
-}
-
-if(FALSE) {
-  # fit to MALLARD data
-  Gamma <- function(X) SE(X) # squared exponential only
-  fit.obj <- fit_to_reference(Gamma)
-  predict.obj <- get_predictions(fit.obj$X, fit.obj$fit)
-  plot_fit(fit.obj$Y, fit.obj$X, fit.obj$labels, predict.obj$Y_predict, predict.obj$X_predict)
+  p
 }
 
 # baboon
 #Gamma <- function(X) PER(X, period=365) # periodic only
 Gamma <- function(X) SE(X) # squared exponential only
-for(indiv in c("DUI", "LEB")) {
-  fit.obj <- fit_to_baboon(Gamma, indiv, start_date_str="2001-10-01", end_date_str="2002-11-30") # wet season through a dry season
-  predict.obj <- get_predictions(fit.obj$X, fit.obj$fit)
-  plot_fit(fit.obj$Y, fit.obj$X, fit.obj$labels, predict.obj$Y, predict.obj$X, paste0("basset_SE_",indiv))
-}
+
+baboon <- "ACA"
+load(paste0("subsetted_indiv_data/",baboon,"_data.RData"))
+
+fit_obj <- fit_to_baboon(baboon, indiv_data, Gamma, date_lower_limit="2001-10-01", date_upper_limit="2003-11-30")
+predict_obj <- get_predictions(fit_obj$X, fit_obj$fit) # interpolates
+plot_predictions(fit_obj, predict_obj, LR_coord=1)
+plot_predictions(fit_obj, predict_obj, LR_coord=2)
 
 
 
